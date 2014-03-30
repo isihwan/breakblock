4eb3a6e2221cc582ef69e29f9b61cae1e8e5c9ab
voider <voider@mylinux.net>
2014-03-30 13:43:23 +0900

    linux comment

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ package sihwan.test
 
 public class test {
 	public static void main(argv[]) {
+		// linux comment
 		System.out.println("hello world");
 		System.out.println("second line");
 		System.out.println("third line");
