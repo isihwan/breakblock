0531daa783ad6bb6a9af0bb129e0c6df1a06a5b6
voider <voider@mylinux.net>
2014-04-03 01:52:28 +0900

    fix correct java syntax

--- src/sub1/Sub1.java
+++ src/sub1/Sub1.java
@@ -1,12 +1,14 @@
 package package sihwan.test.sub1
 
 public class Sub1 {
-	// power of git
-	System.out.println("This is sub1");
-	// edit
-	String edit = "Edit line"; // fix by linux
-	// thanks linux
-	//add source
-	// add 1
-	// add 2
+	public Sub1() {
+		// power of git
+		System.out.println("This is sub1");
+		// edit
+		String edit = "Edit line"; // fix by linux
+		// thanks linux
+		//add source
+		// add 1
+		// add 2
+	}
 }
